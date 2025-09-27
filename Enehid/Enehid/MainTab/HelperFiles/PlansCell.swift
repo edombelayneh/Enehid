cc2e54147622a22414a545c73b5f3b4200864dea
Edom Belayneh <101343364+edombelayneh@users.noreply.github.com>
2025-09-27 13:42:57 -0400

    fixed plans page

--- Enehid/Enehid/MainTab/HelperFiles/PlansCell.swift
+++ Enehid/Enehid/MainTab/HelperFiles/PlansCell.swift
@@ -9,13 +9,30 @@ import UIKit
 
 class PlansCell: UITableViewCell {
 
-    @IBOutlet weak var declineButton: UIButton!
-    @IBOutlet weak var acceptButton: UIButton!
-    @IBOutlet weak var profileUIImageView: UIImageView!
-    @IBOutlet weak var scheduledByLabel: UILabel!
+    
+    @IBOutlet weak var ownerControlsView: UIView?
+    @IBOutlet weak var inviteeStatusView: UIView?
+    @IBOutlet weak var waitingLabel: UILabel?
+    @IBOutlet weak var declineButton: UIButton?
+    @IBOutlet weak var acceptButton: UIButton?
+    
+    
+//    @IBOutlet weak var profileUIImageVIew: UIImageView!
+    @IBOutlet weak var createdBy: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var activityNameLabel: UILabel!
+    
+    var onAccept: (() -> Void)?
+    var onDecline: (() -> Void)?
+    
+    @IBAction func didTapAcceptButton(_ sender: UIButton) {
+        onAccept?()
+    }
+ 
+    @IBAction func didTapDeclineButton(_ sender: UIButton) {
+        onDecline?()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Enehid/Enehid/MainTab/PlansViewController.swift
+++ Enehid/Enehid/MainTab/PlansViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-class PlansViewController: UIViewController {
+class PlansViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return plans.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let plan = plans[indexPath.row]
+        let id = plan.createdByIsMe ? "PlanOwnerCell" : "PlanInviteeCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath) as! PlansCell
 
+        // Common fields
+//        cell.profileUIImageVIew.image = UIImage.enehidLogo
+        cell.activityNameLabel.text = plan.activityName
+        cell.locationLabel.text     = plan.location
+        cell.dateLabel.text         = plan.date
+        cell.createdBy.text    = plan.createdByIsMe ? "Organizer: YOU"
+                                                             : "@\(plan.createdBy)"
+
+        if plan.createdByIsMe {
+            cell.ownerControlsView?.isHidden  = false
+            cell.inviteeStatusView?.isHidden  = true
+            cell.acceptButton?.isHidden       = true
+            cell.declineButton?.isHidden      = true
+            cell.waitingLabel?.text            = "\(plan.acceptedCount)/\(plan.totalCount) friends accepted"
+        } else {
+            cell.ownerControlsView?.isHidden  = true
+            cell.inviteeStatusView?.isHidden  = false
+            cell.acceptButton?.isHidden       = plan.iAccepted
+            cell.declineButton?.isHidden      = !plan.iAccepted
+            cell.waitingLabel?.text           = plan.iAccepted ? "You accepted" : "Waiting on you"
+
+//            cell.onAccept = { [weak self] in /* update model + reload row */ }
+//            cell.onDecline = { [weak self] in /* update model + reload row */ }
+        }
+
+        return cell
+    }
+    
+
+    @IBOutlet weak var plansTableView: UITableView!
+    
+    
+    var plans: [Plans] = mockPlans
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        plansTableView.delegate = self
+        plansTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
