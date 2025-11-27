d4402ef051248034e258562be4a9ce118ea15d97
Edom Belayneh <101343364+edombelayneh@users.noreply.github.com>
2025-11-27 18:55:39 -0500

    fetched picture in bookmark screen

--- Enehid/Enehid/HelperViews/ProfileDetails/Bookmarked/BookmarkCell.swift
+++ Enehid/Enehid/HelperViews/ProfileDetails/Bookmarked/BookmarkCell.swift
@@ -6,38 +6,43 @@
 //
 
 import UIKit
+import SDWebImage
 
 class BookmarkCell: UITableViewCell {
-
+    
     
     @IBOutlet weak var memoryImageView: UIImageView!
-
     
     @IBOutlet weak var recommendButton: UIButton!
+    
+    @IBOutlet weak var addToPlanButton: UIButton!
     @IBOutlet weak var openInMapsButton: UIButton!
-    @IBOutlet weak var commentButton: UIButton!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var detailsLabel: UILabel!
     @IBOutlet weak var bookmarkedButton: UIButton!
     var onRecommendTapped: (() -> Void)?
     var onBookmarkTapped: (() -> Void)?
+    
     func configure(with bookmarked: Memory?) {
         usernameLabel.text = bookmarked?.username ?? "Unknown"
         detailsLabel.text = bookmarked?.caption ?? ""
-//        sharedPostImage.image = feedMemories?.image
         
-//        if let date = comment?.commentDate {
-//            dateLabel.text = DateFormatter.postFormatter.string(from: date)
-//        }
+        // ✅ Load image with SDWebImage
+        if let urlString = bookmarked?.memoryURLs.first,
+           let url = URL(string: urlString) {
+            memoryImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder"))
+        } else {
+            memoryImageView.image = UIImage(named: "placeholder")
+        }
     }
-
+    
     
     @IBAction func onTapRecommend(_ sender: UIButton) {
         onRecommendTapped?()
     }
     @IBAction func onTapOpenInMaps(_ sender: UIButton) {
     }
-    @IBAction func onTapComment(_ sender: UIButton) {
+    @IBAction func onTapAddToPlans(_ sender: UIButton) {
     }
     @IBAction func onTapBookmark(_ sender: UIButton) {
         onBookmarkTapped?()
